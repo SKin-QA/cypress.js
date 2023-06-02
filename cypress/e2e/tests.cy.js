7db3c06764124e9c271494f0a3083b47ba1548f5
Svetlana Kin <132260999+SKin-QA@users.noreply.github.com>
2023-06-02 23:01:40 +0500

    Add files via upload

---
+++ cypress/e2e/tests.cy.js
@@ -0,0 +1,59 @@
+describe('Форма логина', function () {
+	
+	it('Верный логин, верный пароль', function () {
+	    cy.visit('https://login.qa.studio/');
+	    cy.get('#mail').type('german@dolnikov.ru');
+	    cy.get('#pass').type('iLoveqastudio1');
+	    cy.get('#loginButton').click();
+	    cy.contains('Авторизация прошла успешно');
+	    cy.get('#exitMessageButton > .exitIcon');
+	})
+
+	it('Восстановление пароля', function () {
+	    cy.visit('https://login.qa.studio/');
+	    cy.get('#forgotEmailButton').click();
+	    cy.get('#mailForgot').type('german@dolnikov.ru');
+	    cy.get('#restoreEmailButton').click();
+	    cy.contains('Успешно отправили пароль на e-mail');
+	    cy.get('#exitMessageButton > .exitIcon');
+	})
+
+	it('Верный логин, НЕверный пароль', function () {
+	    cy.visit('https://login.qa.studio/');
+	    cy.get('#mail').type('german@dolnikov.ru');
+	    cy.get('#pass').type('iLoveqastudio');
+	    cy.get('#loginButton').click();
+	    cy.contains('Такого логина или пароля нет');
+	    cy.get('#exitMessageButton > .exitIcon');
+	})
+
+	it('НЕверный логин, верный пароль', function () {
+	    cy.visit('https://login.qa.studio/');
+	    cy.get('#mail').type('grman@dolnikov.ru');
+	    cy.get('#pass').type('iLoveqastudio1');
+	    cy.get('#loginButton').click();
+	    cy.contains('Такого логина или пароля нет');
+	    cy.get('#exitMessageButton > .exitIcon');
+	})
+
+	it('Логин без @', function () {
+	    cy.visit('https://login.qa.studio/');
+	    cy.get('#mail').type('germandolnikov.ru');
+	    cy.get('#pass').type('iLoveqastudio1');
+	    cy.get('#loginButton').click();
+	    cy.contains('Нужно исправить проблему валидации');
+	    cy.get('#exitMessageButton > .exitIcon');
+	})
+
+	it('Строчные буквы в логине', function () {
+	    cy.visit('https://login.qa.studio/');
+	    cy.get('#mail').type('GerMan@Dolnikov.ru');
+	    cy.get('#pass').type('iLoveqastudio1');
+	    cy.get('#loginButton').click();
+	    cy.contains('Авторизация прошла успешно');
+	    cy.get('#exitMessageButton > .exitIcon');
+	})
+})
+
+
+
